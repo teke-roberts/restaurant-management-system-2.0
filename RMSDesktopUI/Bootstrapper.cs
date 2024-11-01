fc87d578a5eacf2b9b4b72235b8556653e6fcef5
tekeroberts <tekeroberts@yahoo.com>
2024-11-01 15:58:28 -0400

    adds empty WPF frontend with Caliburn.Micro

---
+++ RMSDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using RMSDesktopUI.Views;
+using RMSDesktopUI.ViewModels;
+
+namespace RMSDesktopUI
+{
+    internal class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
